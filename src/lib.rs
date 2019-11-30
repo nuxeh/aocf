12c95c3833139894c9a99ac38bef078514c03c65
Ed Cragg <drq.11235@gmail.com>
2019-11-30 18:53:41 +0800

    Add some more things

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,28 @@ use failure::Error;
 //mod input;
 //mod submit;
 //mod html;
+mod http;
+
+#[derive(Clone)]
+enum Part {
+    One,
+    Two,
+}
+
+impl Default for Part {
+    fn default() -> Self {
+        Part::One
+    }
+}
 
 #[derive(Default, Clone)]
 struct Aoc {
     year: Option<i32>,
     day: Option<u32>,
-    brief: Option<String>,
+    part: Part,
+    brief: Vec<String>,
     input: Option<String>,
+    solution: Vec<String>
 }
 
 impl Aoc {
@@ -33,11 +48,17 @@ impl Aoc {
         self
     }
 
+    /// Initialise (finish building)
     pub fn init(&mut self) -> Self {
         let now = Utc::now();
         self.year = self.year.or(Some(now.year()));
         self.day = self.day.or(Some(now.day()));
         self.clone()
+    }
+
+    /// Restore the problem from JSON or TOML
+    pub fn restore(&mut self, path: impl AsRef<Path>) -> Self {
+        Self::default()
     }
 
     /// Get the problem brief as HTML and sanitise it to plain text
@@ -53,5 +74,30 @@ impl Aoc {
     /// Submit the solution
     pub fn submit() -> Result<(), Error> {
         Ok(())
+    }
+
+    /// get a JSON representation for the AoC problem
+    pub fn as_json() -> String {
+        "".to_string()
+    }
+
+    /// get a TOML representation for the AoC problem
+    pub fn as_toml() -> String {
+        "".to_string()
+    }
+
+    /// Save JSON to path
+    pub fn write_json(&self, path: impl AsRef<Path>) {
+
+    }
+
+    /// Save TOML to path
+    pub fn write_toml(&self, path: impl AsRef<Path>) {
+
+    }
+
+    /// Get time until release
+    pub fn get_time_until_release() {
+
     }
 }
