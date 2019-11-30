882bc9b37acbb0a4121cb43bb382fa9599a50bc0
Ed Cragg <drq.11235@gmail.com>
2019-11-30 23:21:50 +0800

    Refactor getting url

--- src/http.rs
+++ src/http.rs
@@ -7,15 +7,16 @@ use reqwest::header::COOKIE;
 
 const BASE: &str = "https://adventofcode.com";
 
-fn get_url(y: i32, d: u32) -> String {
-    format!("{}/{}/day/{}", BASE, y, d)
+fn get_url(aoc: &Aoc) -> Result<String, Error> {
+    let url = match (aoc.day, aoc.year) {
+        (Some(d), Some(y)) => format!("{}/{}/day/{}", BASE, y, d),
+        _ => bail!("day or year not initialised"),
+    };
+    Ok(url)
 }
 
 pub fn get_input(aoc: &Aoc) -> Result<String, Error> {
-    let url = match (aoc.day, aoc.year) {
-        (Some(d), Some(y)) => format!("{}/input", get_url(y, d)),
-        _ => bail!("day or year not initialised"),
-    };
+    let url = format!("{}/input", get_url(aoc)?);
     let cookie = format!("session={}", aoc.cookie);
     let input = Client::new()
         .get(&url)
