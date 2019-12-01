3e7498413d85b4bd48730df2d12644ae9f254d20
Ed Cragg <drq.11235@gmail.com>
2019-12-01 09:31:51 +0800

    Add a binary

--- aocf_cli/src/bin/aocf.rs
+++ aocf_cli/src/bin/aocf.rs
@@ -1,3 +1,34 @@
+#[macro_use] extern crate log;
+#[macro_use] extern crate failure;
+#[macro_use] extern crate serde_derive;
+extern crate aocf;
+extern crate docopt;
+extern crate toml;
+extern crate slog;
+
+use docopt::Docopt;
+use failure::Error;
+
+const USAGE: &str = "
+Advent of Code Swiss army knife.
+
+Usage:
+    aocf [options]
+
+Options:
+    -h --help           Show this help message.
+    --version           Print version.
+";
+
+#[derive(Deserialize)]
+struct Cliargs {
+
+}
+
 fn main() {
+    let args: Cliargs = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
     println!("Hello, world!");
 }
