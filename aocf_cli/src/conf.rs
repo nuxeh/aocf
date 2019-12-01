ad52ab1bd780e9c68d5cf315a222e883e0a806b6
Ed Cragg <drq.11235@gmail.com>
2019-12-01 19:24:48 +0800

    Add conf module

---
+++ aocf_cli/src/conf.rs
@@ -0,0 +1,25 @@
+use failure::Error;
+use std::fs::{File, read_to_string};
+use std::io::Write;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+pub struct Conf {
+    editor: String,
+    cookie: String,
+    year: i32,
+}
+
+impl Conf {
+    pub fn load(path: impl AsRef<Path>) -> Result<Self, Error> {
+        let conf = read_to_string(path.as_ref())?;
+        let conf: Self = toml::de::from_str(&conf)?;
+        Ok(conf)
+    }
+
+    pub fn write(&self, path: impl AsRef<Path>) -> Result<(), Error> {
+        let mut file = File::create(path)?;
+        file.write_all(toml::ser::to_string(&self)?.as_bytes())?;
+        Ok(())
+    }
+}
