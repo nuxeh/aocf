92b6c7002f12355275e54673cf5bdc0865eb53e5
Ed Cragg <drq.11235@gmail.com>
2019-12-12 23:15:24 +0800

    Add example

---
+++ examples/day01.rs
@@ -0,0 +1,20 @@
+extern crate aocf;
+
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2019))
+        .day(Some(1))
+        .cookie_file("./examples/cookie")
+        .init()
+        .unwrap();
+
+    let input = if let Ok(i) = aoc.get_input() {
+        i
+    } else {
+        "you probably need to add a valid cookie".to_string()
+    };
+
+    println!("{}", input);
+}
